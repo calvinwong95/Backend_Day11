0a61b890613910bfee38a2be65c7788802ad4701
calvinwong95 <calvinwongch95@gmail.com>
2021-10-25 13:42:40 +0800

    v3

--- db.sql.php
+++ db.sql.php
@@ -1,7 +1,7 @@
 <?php
     $servername = "localhost";
     $serverUsername = "Cheehoe";
-    $serverPassword = "test124";
+    $serverPassword = "test123";
     $dBname = "mysejahteri";
 
     $conn = mysqli_connect($servername,$serverUsername,$serverPassword,$dBname);
